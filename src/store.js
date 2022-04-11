129401682f85eaa1a7f1fb91500b6c5b134685dd
Lisa Chatellier <78473267+shinboram-90@users.noreply.github.com>
2022-04-11 11:43:23 +0200

    Updates

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import filmsReducer from "./reducers/films";
-
-export const store = configureStore({
-	reducer: {
-		films: filmsReducer,
-	},
-});
